af1cd7684095e7e5035fbd51c4390c4da57eb1cf
joohwancode <jhpark4947@gmail.com>
2023-03-31 23:48:51 +0900

    가방문제

--- DP/9.알리바바.py
+++ DP/9.알리바바.py
@@ -1,23 +1,23 @@
-import sys
+# import sys
 
-sys.stdin = open("DP/input.txt", "rt")
+# sys.stdin = open("DP/input.txt", "rt")
 
-def DFS(x,y):
-    if dy[x][y]>0:
-        return dy[x][y]
-    if x==0 and y==0:
-        return arr[0][0]
-    else:
-        if y==0:
-            dy[x][y]=DFS(x-1,y)+arr[x][y]
-        elif x==0:
-            dy[x][y]=DFS(x,y-1)+arr[x][y]
-        else:
-            dy[x][y]=min(DFS(x-1,y),DFS(x,y-1))+arr[x][y]
-        return dy[x][y]
-    ###메모이제이션 해야한다는 것을 주의해라 ! 
-if __name__=="__main__":
-    n=int(input())
-    arr=[list(map(int,input().split())) for _ in range(n)]
-    dy=[[0]*n for _ in range(n)]
-    print(DFS(n-1,n-1))
+# def DFS(x,y):
+#     if dy[x][y]>0:
+#         return dy[x][y]
+#     if x==0 and y==0:
+#         return arr[0][0]
+#     else:
+#         if y==0:
+#             dy[x][y]=DFS(x-1,y)+arr[x][y]
+#         elif x==0:
+#             dy[x][y]=DFS(x,y-1)+arr[x][y]
+#         else:
+#             dy[x][y]=min(DFS(x-1,y),DFS(x,y-1))+arr[x][y]
+#         return dy[x][y]
+#     ###메모이제이션 해야한다는 것을 주의해라 ! 
+# if __name__=="__main__":
+#     n=int(input())
+#     arr=[list(map(int,input().split())) for _ in range(n)]
+#     dy=[[0]*n for _ in range(n)]
+#     print(DFS(n-1,n-1))
