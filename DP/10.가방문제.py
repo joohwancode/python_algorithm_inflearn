03927496e6197cee5496dcbba6a374c299914e2e
joohwancode <jhpark4947@gmail.com>
2023-04-01 15:49:28 +0900

    DP.동전문제

--- DP/10.가방문제.py
+++ DP/10.가방문제.py
@@ -1,12 +1,12 @@
-import sys
+# import sys
 
-sys.stdin = open("DP/input.txt", "rt")
+# sys.stdin = open("DP/input.txt", "rt")
 
-if __name__=="__main__":
-    n,m=map(int,input().split())
-    dy=[0]*(m+1)
-    for i in range(n):
-        w,v=map(int,input().split())
-        for j in range(w,m+1):
-            dy[j]=max(dy[j],dy[j-w]+v)
-    print(dy[m])
+# if __name__=="__main__":
+#     n,m=map(int,input().split())
+#     dy=[0]*(m+1)
+#     for i in range(n):
+#         w,v=map(int,input().split())
+#         for j in range(w,m+1):
+#             dy[j]=max(dy[j],dy[j-w]+v)
+#     print(dy[m])
